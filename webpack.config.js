77f46aef2535523c952d20386d66579214173d55
Thomas Lovett <tklovett@gmail.com>
2017-07-03 13:22:39 -0500

    Upgrade to Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
-var webpackTargetElectronRenderer = require('webpack-target-electron-renderer')
 
-var options = {
+module.exports = {
     // devtool: 'eval-source-map',
     entry: {
         vendor: [
@@ -18,64 +17,71 @@ var options = {
     },
     output: {
         filename: "[name].js",
-        path: './dist'
+        path: path.join(__dirname, 'dist'),
     },
     resolve: {
-        root: path.resolve(__dirname) + '/src',
-        extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+        modules: [
+            path.join(__dirname, 'src'),
+            'node_modules'
+        ],
+        extensions: ['.webpack.js', '.web.js', '.js', '.jsx']
     },
     module: {
-        loaders: [
-            {
-                test: /\.json/,
-                loader: require.resolve('json-loader')
-            },
+        rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: require.resolve('babel-loader'),
+                loader: 'babel-loader',
                 query: {
                     presets: [
-                        require.resolve('babel-preset-es2015'),
-                        require.resolve('babel-preset-react')
+                        'babel-preset-es2015',
+                        'babel-preset-react'
                     ],
                     plugins: [
-                        require.resolve('babel-plugin-transform-class-properties'),
-                        require.resolve('babel-plugin-syntax-async-functions'),
-                        require.resolve('babel-plugin-transform-regenerator'),
-                        require.resolve('babel-plugin-transform-object-rest-spread')
+                        'babel-plugin-transform-class-properties',
+                        'babel-plugin-syntax-async-functions',
+                        'babel-plugin-transform-regenerator',
+                        'babel-plugin-transform-object-rest-spread'
                     ]
                 }
             },
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
             {
-                test: /\.png$/, loader: "url-loader?limit=100000"
+                test: /\.png$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000
+                    }
+                }]
             },
             {
-                test: /\.jpg$/, loader: "file-loader"
+                test: /\.jpg$/,
+                loader: 'file-loader'
             }
         ]
     },
+    target: 'electron-renderer',
     plugins: [
         new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(en|nl)$/),
         new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendor'],
+            name: 'vendor',
             filename: "[name].js"
         }),
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery' // maybe unnecessary, since this ins't Angular 1: https://webpack.js.org/plugins/provide-plugin/#usage-jquery-with-angular-1
         }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        })
+        }),
+        new webpack.IgnorePlugin(/vertx/)
     ]
 }
-
-options.target = webpackTargetElectronRenderer(options)
-
-module.exports = options
